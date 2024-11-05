2219a3a231d8951921b880bc507ccaffae4947de
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-05 19:23:38 +0600

    feat: first check written

--- section-three/scenarios.js
+++ section-three/scenarios.js
@@ -1,8 +1,15 @@
 import http from 'k6/http';
-import check from 'k6';
+import { check } from 'k6';
 
 export default function () {
   const res = http.get('https://test.k6.io');
 
   console.log(res.status);
+  check(true, {
+    'true is true': (value) => value === true
+  })
+
+  check(false, {
+    'false is true': (value) => value === true
+  })
 }
