fc126a13684ca850e1514d3d65d319f16f5dd80c
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-07 11:14:15 +0600

    feat: video on checks thresholds completed

--- section-three/scenarios.js
+++ section-three/scenarios.js
@@ -1,17 +1,25 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
+import exec from "k6/execution";
 
 export const options = {
   vus: 10,
-  duration: '10s',
+  duration: "10s",
   thresholds: {
-    http_req_duration: ['p(95)<500'],
-    http_req_failed: ['rate<0.01'],
-  }
-}
+    http_req_duration: ["p(95)<500"],
+    // http_req_duration: ["p(95)<500", "max<1000"],
+    http_req_failed: ["rate<=0.02"],
+    // http_reqs: ['count>20', 'rate>5'],
+    // vus: ['value>9']
+    checks: ["rate>=0.98"],
+  },
+};
 
 export default function () {
-  const res = http.get('https://test.k6.io');
+  const res = http.get(
+    `https://test.k6.io${exec.scenario.iterationInTest === 1 ? "/foo" : ""}`
+  );
+  console.log(exec.scenario.iterationInTest);
 
   // const res = http.get('https://test.k6.io/contacts.php');
 
@@ -26,8 +34,9 @@ export default function () {
 
   // * Status checks
   check(res, {
-    'status is 200': (r) => r.status === 200,
-    'page is startPage': (r) => r.body.includes('Collection of simple web-pages')
+    "status is 200": (r) => r.status === 200,
+    "page is startPage": (r) =>
+      r.body.includes("Collection of simple web-pages"),
   });
   sleep(2);
 }
